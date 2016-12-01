0b4aea4c05aa4e5b00230c0f57b9520dbb7881e4
donganqing <2356101330@qq.com>
2016-12-01 22:15:39 +0800

    add some classic case

---
+++ C3登录效果/js/jquery.js
@@ -0,0 +1,46 @@
+	$(document).ready(function(){
+	//var x = 150;
+	//var y = 50;
+	//var rand = parseInt(Math.random() * (x - y + 1) + y);
+	////alert(rand);
+	//$(".f div").css("width",rand);
+	//$(".f div").css("height",rand);
+		$(".submit").click(function(){
+			$(".b").css("transform","translate(0px,-260px)");
+			$(".b").css("opacity","0");
+			$("h2").css("transform","translate(0px,130px)");
+			$("h2 span").css("opacity","1");
+			$("h2 span i").css("animation-play-state","running");
+			//window.setTimeout(function(){ location.href="https://www.baidu.com/";}, 5000);
+		});
+		
+		
+		//$(".submit").click(function(){
+		//	function show(){
+		//		$(".b").css("transform","translate(0px,-260px)");
+		//		$(".b").css("opacity","0");
+		//		$("h2").css("transform","translate(0px,130px)");
+		//		$("h2 span").css("opacity","1");
+		//		$("h2 span i").css("animation-play-state","running");
+		//		//$(".b").css("display","none");
+		//		}
+		//		setInterval(show,5000);
+		//});
+		//—”≥Ÿ÷¥––JS
+	//$(function(){
+	//function show(){
+	//  $("h2").css("transform","translate(0px,130px)");
+	//  $("h2").css("transform","translate(0px,130px)");
+	//}
+	//setInterval(show,5000);
+	//});
+
+
+		//$("input").onfocus(function(){
+		//	$(this).css("width","220px");
+		//});
+		//$("input").onblur(function(){
+		//	$(this).css("width","180px");
+		//});
+		
+	});
